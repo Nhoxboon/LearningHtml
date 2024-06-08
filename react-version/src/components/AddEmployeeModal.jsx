c161723f2d32e7f81515119dcc2992ac8831d09b
Nhoxboon <trannhatanh156@gmail.com>
2024-06-09 03:14:01 +0700

    up

--- react-version/src/components/AddEmployeeModal.jsx
+++ react-version/src/components/AddEmployeeModal.jsx
@@ -1,33 +1,38 @@
-import React, {useState} from 'react';
-import 'bootstrap/js/dist/modal';
+import React, { useState } from 'react';
+
 
 function AddEmployeeModal(props) {
+    const validatePhoneNumber = (phone) => /^0\d{9}$/.test(phone);
 
-    function validatePhoneNumber(phone) {
-        return /^0\d{9}$/.test(phone);
-    }
+    const [newEmployee, setNewEmployee] = useState({
+        name: '',
+        email: '',
+        address: '',
+        phone: ''
+    });
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
-    const [phone, setPhone] = useState('');
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setNewEmployee({ ...newEmployee, [name]: value });
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        const { name, email, address, phone } = newEmployee;
         if (!name || !email || !address || !validatePhoneNumber(phone)) {
             alert("Vui lòng kiểm tra lại thông tin. Số điện thoại phải có độ dài 10 ký tự và bắt đầu bằng số 0.");
             return;
         }
-        const newEmployee = { name, email, address, phone };
         props.addEmployee(newEmployee);
-        setName('');
-        setEmail('');
-        setAddress('');
-        setPhone('');
+        setNewEmployee({
+            name: '',
+            email: '',
+            address: '',
+            phone: ''
+        });
+        document.getElementById('addEmployeeModal').classList.remove('show'); // Loại bỏ lớp 'show' để đóng modal
+        document.getElementById('addEmployeeModal').setAttribute('aria-hidden', 'true'); // Đặt thuộc tính aria-hidden thành 'true' để đóng modal
     };
-    document.getElementById('addEmployeeModal').classList.remove('show'); // Loại bỏ lớp 'show' để đóng modal
-    document.getElementById('addEmployeeModal').setAttribute('aria-hidden', 'true'); // Đặt thuộc tính aria-hidden thành 'true' để đóng modal
-    
 
     return (
         <div className="modal fade" id="addEmployeeModal" tabIndex="-1" aria-labelledby="addEmployeeModalLabel" aria-hidden="true">
@@ -41,19 +46,19 @@ function AddEmployeeModal(props) {
                         <div className="modal-body">
                             <div className="mb-3">
                                 <label htmlFor="employeeName" className="form-label">Họ và Tên</label>
-                                <input type="text" className="form-control" id="employeeName" value={name} onChange={(e) => setName(e.target.value)} required />
+                                <input type="text" className="form-control" id="employeeName" name="name" value={newEmployee.name} onChange={handleInputChange} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="employeeEmail" className="form-label">Thư điện tử</label>
-                                <input type="email" className="form-control" id="employeeEmail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                                <input type="email" className="form-control" id="employeeEmail" name="email" value={newEmployee.email} onChange={handleInputChange} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="employeeAddress" className="form-label">Địa chỉ</label>
-                                <textarea className="form-control" id="employeeAddress" value={address} onChange={(e) => setAddress(e.target.value)} required rows="2"></textarea>
+                                <textarea className="form-control" id="employeeAddress" name="address" value={newEmployee.address} onChange={handleInputChange} required rows="2"></textarea>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="employeePhone" className="form-label">Số điện thoại</label>
-                                <input type="text" className="form-control" id="employeePhone" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                                <input type="text" className="form-control" id="employeePhone" name="phone" value={newEmployee.phone} onChange={handleInputChange} required />
                             </div>
                         </div>
                         <div className="modal-footer bg-secondary">
